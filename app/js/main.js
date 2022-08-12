e4b0eb5cb37e7eb0ffb7adaf5cf375b8407710dd
Maria <m1a2r3.kovalenko@gmail.com>
2022-08-12 10:42:47 +0300

    открытие мобильного меню

--- app/js/main.js
+++ app/js/main.js
@@ -34,55 +34,37 @@ document
     this.classList.toggle("active");
   });
 
-// const buttonOpenMenu = document.querySelector(".header__button-menu");
-// const buttonConditionClosed = document.querySelector(".header__toggle--closed");
-// // const buttonConditionOpened = document.querySelector(".header__toggle--opened");
-// const menu = document.querySelector(".header__navigation");
+const buttonMenuBurger = document.querySelector(".header__button-menu");
+const buttonviewСross = document.querySelector(".header__toggle--opened");
+const headerWrapper = document.querySelector(".header__wrapper");
+const menu = document.querySelector(".mobile-menu");
 
-// const headerWrapper = document.querySelector(".header__wrapper");
-// // const headerWrapperPseudo = document.querySelector(".header__wrapper::before");
-// const logo = document.querySelector(".header__logo");
-// const search = document.querySelector(".header__search");
-// const social = document.querySelector(".header__social");
-// const contacts = document.querySelector(".header__contacts");
+const contentMain = document.querySelector(".page-main");
+const contentFooter = document.querySelector(".page-footer");
 
-// const contentMain = document.querySelector(".page-main");
-// const contentFooter = document.querySelector(".page-footer");
+let condition = "close";
 
-// let condition = "close";
+buttonMenuBurger.addEventListener("click", function () {
+  if (condition === "close") {
+    menu.style.display = "block";
+    headerWrapper.style.display = "none";
+    contentMain.style.display = "none";
+    contentFooter.style.display = "none";
+    condition = "open";
+  }
+});
 
-// buttonOpenMenu.addEventListener("click", function () {
-//   if (condition === "close") {
-//     menu.style.display = "block";
-//     buttonConditionClosed.classList.remove("header__toggle--closed");
-//     buttonConditionClosed.classList.add("header__toggle--opened");
-//     contentMain.style.display = "none";
-//     contentFooter.style.display = "none";
-//     logo.style.display = "none";
-//     search.style.display = "block";
-//     // headerWrapperPseudo.style.display = "none";
-//     headerWrapper.style.display = "flex";
-//     headerWrapper.style.justifyContent = "space-Between";
-//     social.style.display = "none";
-//     contacts.style.display = "none";
+buttonviewСross.addEventListener("click", function () {
+  if (condition === "open") {
+    menu.style.display = "none";
+    headerWrapper.style.display = "grid";
+    contentMain.style.display = "block";
+    contentFooter.style.display = "block";
 
-//     condition = "open";
-//   }
-// });
+    condition = "close";
 
-// buttonOpenMenu.addEventListener("click", function () {
-//   if (condition === "open") {
-//     buttonConditionClosed.classList.remove("header__toggle--opened");
-//     buttonConditionClosed.classList.add("header__toggle--closed");
-
-//     menu.classList.remove("showMenu");
-//     contentMain.style.display = "block";
-//     contentFooter.style.display = "block";
-
-//     condition = "close";
-
-//     // document.querySelectorAll(".navigation__list .open").forEach((element) => {
-//     //   element.classList.remove("open");
-//     // });
-//   }
-// });
+    // document.querySelectorAll(".navigation__list .open").forEach((element) => {
+    //   element.classList.remove("open");
+    // });
+  }
+});
